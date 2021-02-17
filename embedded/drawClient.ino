8ca0ac256f01e5f37084296e8e94839349f479a0
Nitek <nitek@blickt.es>
2021-02-17 21:33:25 +0100

    Migrate to AsyncMQTT and optimize protocol by reducing color depth and adding full matrix update

--- embedded/drawClient.ino
+++ embedded/drawClient.ino
@@ -1,5 +1,9 @@
 #include <WiFiManager.h>
-#include <PubSubClient.h>
+extern "C" {
+	#include "freertos/FreeRTOS.h"
+	#include "freertos/timers.h"
+}
+#include <AsyncMqttClient.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
@@ -14,14 +18,16 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, PIN,
   NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
   NEO_GRB            + NEO_KHZ800);
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-String clientId;
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
 
 const char DRAW_TOPIC[] = "nitek/draw";
 const char CONNECT_TOPIC[] = "nitek/draw/connect";
 
-uint8_t data[16][16][3] = {0};
+uint16_t data[8][8] = {0};
+
+volatile bool updateMatrix = false;
 
 void startWifi() {
   Serial.println("Connecting Wifi");
@@ -40,8 +46,86 @@ void startWifi() {
     wifiManager.setEnableConfigPortal(true);
     wifiManager.autoConnect("draw", "drawdraw");
   }
+}
 
-  Serial.print("WiFi connected with IP: "); Serial.println(WiFi.localIP());
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void WiFiEvent(WiFiEvent_t event) {
+    Serial.printf("[WiFi-event] event: %d\n", event);
+    switch(event) {
+    case SYSTEM_EVENT_STA_GOT_IP:
+        Serial.println("WiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+        connectToMqtt();
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        Serial.println("WiFi lost connection");
+        xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+		    xTimerStart(wifiReconnectTimer, 0);
+        break;
+    }
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  mqttClient.subscribe(CONNECT_TOPIC, 1);
+  mqttClient.subscribe(DRAW_TOPIC, 1);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t length, size_t index, size_t total) {
+  Serial.print("Message arrived on topic: ");
+  Serial.println(topic);
+
+  if (strcmp(topic, DRAW_TOPIC) == 0) {
+    if(length == 3) {
+      uint8_t x = payload[0] >> 4;
+      uint8_t y = payload[0] & 0x0F;
+
+      if(x >= matrix.width() || y >= matrix.height()) {
+        Serial.printf("Invalid coordinates %d,%d\n", x, y);
+        return;
+      }
+
+      uint16_t color = payload[1] * 256 + payload[2];
+
+      if(data[x][y] != color) {
+        data[x][y] = color;
+
+        matrix.drawPixel(x, y, color);
+        updateMatrix = true;
+      }
+    } else if (length == matrix.width()*matrix.height()*2) {
+      for(uint8_t x = 0; x < matrix.width(); x++) {
+        for(uint8_t y = 0; y < matrix.height(); y++) {
+          uint8_t i = 2*(x + (y*matrix.width()));
+          uint16_t color = payload[i] * 256 + payload[i+1];
+          matrix.drawPixel(x, y, color);
+        }
+      }
+      updateMatrix = true;
+    } else {
+      Serial.print("Wrong msg size: ");
+      Serial.println(length);
+      return;
+    }
+  } else if (strcmp(topic, CONNECT_TOPIC) == 0) {
+    sync();
+  } else {
+    Serial.print("Wrong topic?! ");
+    Serial.println(topic);
+  }
 }
 
 void setup() {
@@ -53,91 +137,37 @@ void setup() {
   matrix.fill(0);
   matrix.show();
 
+  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(startWifi));
+
+  WiFi.onEvent(WiFiEvent);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
+
   startWifi();
 
-  String clientId = "ESP32Client-";
-  clientId += String(random(0xffff), HEX);
-
-  client.setServer(MQTT_SERVER, MQTT_PORT);
-  client.setCallback(callback);
 }
 
 void sync() {
   Serial.print("Sync: ");
-  uint8_t payload[5];
+  char payload[8*8*2];
   for(uint8_t x = 0; x < matrix.width(); x++) {
     for(uint8_t y = 0; y < matrix.height(); y++) {
-      if(data[x][y][0] == 0 && data[x][y][1] == 0 && data[x][y][2] == 0) {
-        Serial.print(".");
-        continue;
-      }
-      payload[0] = x;
-      payload[1] = y;
-      payload[2] = data[x][y][0];
-      payload[3] = data[x][y][1];
-      payload[4] = data[x][y][2];
-      client.publish(DRAW_TOPIC, payload, sizeof(payload));
-      Serial.print(",");
+      uint8_t i = 2*(x + (y*matrix.width()));
+      payload[i] = data[x][y] >> 8;
+      payload[i+1] = data[x][y] & 0xFF;
     }
   }
+  mqttClient.publish(DRAW_TOPIC, 1, false, payload, sizeof(payload));
   Serial.println();
 }
 
-void callback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Message arrived on topic: ");
-  Serial.println(topic);
-
-  if (strcmp(topic, DRAW_TOPIC) == 0) {
-    if(length != 5) {
-      Serial.print("Wrong msg size: ");
-      Serial.println(length);
-      return;
-    }
-    
-    uint8_t x = message[0];
-    uint8_t y = message[1];
-    uint16_t color = matrix.Color(message[2], message[3], message[4]);
-
-    if(x >= matrix.width() || y >= matrix.height()) {
-      Serial.printf("Invalid coordinates %d,%d\n", x, y);
-      return;
-    }
-
-    data[x][y][0] = message[2];
-    data[x][y][1] = message[3];
-    data[x][y][2] = message[4];
-
-    matrix.drawPixel(x, y, color);
-    matrix.show();
-  } else if (strcmp(topic, CONNECT_TOPIC) == 0) {
-    sync();
-  } else {
-    Serial.print("Wrong topic?! ");
-    Serial.println(topic);
-  }
-}
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      client.subscribe(DRAW_TOPIC);
-      client.subscribe(CONNECT_TOPIC);
-      sync();
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      delay(5000);
-    }
-  }
-}
-
 void loop() {
-  if (!client.connected()) {
-    reconnect();
+  if(updateMatrix) {
+    matrix.show();
+    updateMatrix = false;
   }
-  client.loop();
-
 }
