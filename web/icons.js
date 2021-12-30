abca20647e7f360fb09168a4cd93a6eb84bcf98e
el-paso <mister.fraas@gmail.com>
2021-12-29 16:28:24 -0800

    First version of the feature send icons

---
+++ web/icons.js
@@ -0,0 +1,39 @@
+const icons = [
+    {
+      "iconPath": "icons/twitter.png",
+      "iconTitle": "Twitter"
+    },
+    {
+      "iconPath": "icons/dog.png",
+      "iconTitle": "Dog"
+    },
+    {
+      "iconPath": "icons/fire.png",
+      "iconTitle": "Fire"
+    },
+    {
+      "iconPath": "icons/cry.png",
+      "iconTitle": "Crying"
+    },
+    {
+      "iconPath": "icons/laugh.png",
+      "iconTitle": "Laughing"
+    },
+    {
+      "iconPath": "icons/smile.png",
+      "iconTitle": "Smiling"
+    },
+    {
+      "iconPath": "icons/world.png",
+      "iconTitle": "World"
+    },
+    {
+      "iconPath": "icons/dead.png",
+      "iconTitle": "Skull"
+    },
+    {
+      "iconPath": "icons/love.png",
+      "iconTitle": "Heart"
+    }
+
+  ]
