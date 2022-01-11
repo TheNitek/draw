142a8f43c47a8291ab2c90c3e78bde2348280733
el-paso <34002966+el-paso@users.noreply.github.com>
2022-01-11 20:03:33 +0100

    First version of the feature send icons (#1)
    Send icons to matrix

---
+++ web/icons.js
@@ -0,0 +1,39 @@
+const icons = [
+    {
+      "iconPath": "icons/cat.png",
+      "iconTitle": "Cat"
+    },
+    {
+      "iconPath": "icons/dino.png",
+      "iconTitle": "Dinosaur"
+    },
+    {
+      "iconPath": "icons/dog.png",
+      "iconTitle": "Dog"
+    },
+    {
+      "iconPath": "icons/duck.png",
+      "iconTitle": "Duck"
+    },
+    {
+      "iconPath": "icons/man.png",
+      "iconTitle": "Man"
+    },
+    {
+      "iconPath": "icons/monster.png",
+      "iconTitle": "Monster"
+    },
+    {
+      "iconPath": "icons/plant.png",
+      "iconTitle": "Plant"
+    },
+    {
+      "iconPath": "icons/robot.png",
+      "iconTitle": "Robot"
+    },
+    {
+      "iconPath": "icons/yoda.png",
+      "iconTitle": "Yoda"
+    }
+
+  ]
